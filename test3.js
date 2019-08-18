9183d58537021fb8c0d3a7b41ab2a0a7ea4dc98b
徐文昊 <48832872+WindyValley@users.noreply.github.com>
2019-08-18 21:20:43 +0800

    第三测试页的一些调整
    test3.js保留函数定义，载入位置移动到<head>标签。创建test3_1.js进行事件绑定，载入位置位于<body>标签末尾。加入按钮示例，单机时显示当前详细时间。

--- test3.js
+++ test3.js
@@ -1,6 +1,9 @@
-var img = document.getElementById('img1');
-img.onmouseover = function (ev) {
-    console.log("单击");
+function img1_mouseover() {
+    var img = document.getElementById("img1");
     img.style.left = Math.floor(Math.random() * 1000)+"px";
-    img.style.top = Math.floor(Math.random() * 500) + "px";
+    img.style.top = Math.floor(Math.random() * 500 + 20) + "px";
+}
+function flushdate_mouseclick() {
+    var date = document.getElementById("date");
+    date.innerHTML = Date();
 }
---
+++ test3_1.js
@@ -0,0 +1,2 @@
+document.getElementById("img1").onmouseover = function (ev) { img1_mouseover(); };
+document.getElementById("flushdate").onclick = function (ev) { flushdate_mouseclick(); };
